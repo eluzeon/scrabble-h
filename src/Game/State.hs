a3d66955e7148d67d31b8e83295ffedf3b819b63
europestar <makarov.tim.dev@gmail.com>
2019-11-27 01:01:10 +0300

    Refactored Point data type

--- src/Game/State.hs
+++ src/Game/State.hs
@@ -1,10 +1,2 @@
 module Game.State where
 
-import Game.Types
-
-initGame :: GameState
-initGame = Game (map Left [x | x <- [0..254]]) First
-
-
-availablePoints :: Board -> [Int]
-availablePoints board = [x | Left x <- board]
--- src/Game/Types.hs
+++ src/Game/Types.hs
@@ -2,8 +2,14 @@ module Game.Types where
 
 import Data.List
 
--- Point is Free (if Int) or Filled (with Sybmol)
-type Point = Either Int Symbol
+
+data Point 
+    = Simple -- Green field
+    | DoubleLetter -- Light blue double points for letter
+    | DoubleWord -- Pink double points for word
+    | TripleLetter -- Blue tripple points for letter
+    | TrippleWord -- Red tripple p. for word
+
 type Board = [Point]
 
 -- Symbols which can be set into field point
@@ -24,6 +30,7 @@ data Symbol
     | N 
     | O 
     | P
+    | Q
     | R
     | S
     | T
@@ -35,5 +42,35 @@ data Symbol
     | Z
     deriving (Show, Eq)
 
+
+sybmolsPoints = [
+    (A, 1),
+    (B, 3),
+    (C, 3),
+    (D, 2),
+    (E, 1),
+    (F, 4),
+    (G, 2),
+    (H, 4),
+    (I, 1),
+    (J, 8),
+    (K, 5),
+    (L, 1),
+    (M, 3),
+    (N, 1),
+    (O, 1),
+    (P, 3),
+    (Q, 10),
+    (R, 1),
+    (S, 1),
+    (T, 1),
+    (U, 1),
+    (V, 4),
+    (W, 4),
+    (X, 8),
+    (Y, 4),
+    (Z, 10)]    
+
 data Player = First | Second
+
 data GameState = Game { board :: Board, player :: Player }
